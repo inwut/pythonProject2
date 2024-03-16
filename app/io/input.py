8ed84210474e2082ce9697920b52840787b3444b
Daria Vetrykush <dariavetrykush@gmail.com>
2024-03-16 23:22:12 +0200

    Define functions

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+
+def get_input_from_console():
+    pass
+
+
+def read_data_from_file(path):
+    pass
+
+
+def read_data_from_csv(path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,11 @@
+
+def output_to_console(text):
+    pass
+
+
+def write_text_to_file(text, path):
+    pass
+
+
+def write_text_to_csv(text, path):
+    pass
