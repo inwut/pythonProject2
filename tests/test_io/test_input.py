3fdf6a84d4f138b44acb9d2e71fa832b357902c7
Daria Vetrykush <dariavetrykush@gmail.com>
2024-03-17 12:17:23 +0200

    Add tests for read_data_from_csv function

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -21,3 +21,22 @@ def test_read_data_from_file_with_csv():
     actual = read_data_from_file(PATH_TO_CSV_FILE)
     expected = "Name,HEX,RGB\nWhite,#FFFFFF,\"rgb(100,100,100)\"\nSilver,#C0C0C0,\"rgb(75,75,75)\""
     assert actual == expected
+
+
+def test_read_data_from_csv():
+    actual = read_data_from_csv(PATH_TO_CSV_FILE)
+    expected = pd.DataFrame([["White", "#FFFFFF", "rgb(100,100,100)"],
+                             ["Silver", "#C0C0C0", "rgb(75,75,75)"]],
+                            columns=["Name", "HEX", "RGB"])
+    pd.testing.assert_frame_equal(expected, actual)
+
+
+def test_read_data_from_csv_file_raise_exception():
+    with pytest.raises(FileNotFoundError):
+        read_data_from_csv("non_existing_file")
+
+
+def test_read_data_from_csv_with_text_file():
+    actual = read_data_from_csv(PATH_TO_TXT_FILE)
+    expected = pd.DataFrame(columns=["Most individuals don't think about numbers"])
+    pd.testing.assert_frame_equal(expected, actual)
