8f87c373d7f791b468106e672169a21f5521b02e
Daria Vetrykush <dariavetrykush@gmail.com>
2024-03-18 18:22:19 +0200

    Fix docstrings

--- app/io/output.py
+++ app/io/output.py
@@ -18,9 +18,6 @@ def write_text_to_file(text, path):
     Args:
          text (str): text to write to file.
          path (str): path to file to write text to.
-
-    Raises:
-        FileNotFoundError: if file does not exist.
     """
     with open(path, 'a') as f:
         f.write(text)
@@ -33,8 +30,5 @@ def write_text_to_csv(data_frame, path):
     Args:
         data_frame (pd.DataFrame): data frame to write to file.
         path (str): path to file to write data frame to.
-
-    Raises:
-        FileNotFoundError: if file does not exist.
     """
     data_frame.to_csv(path, mode='a')
